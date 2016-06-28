efc5bccf7ba5bf44c48f9dc8ddbab0190c9a307b
CrysthianCunhaLP2 <crysthiancunha2015@gmail.com>
2016-06-28 15:36:45 -0300

    trablp2

---
+++ Trab(1)Lp2/ConsoleApplication1/Ponto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Ponto
+    {
+        public double X {get; set;}
+        public double Y { get; set; }
+        public Ponto() {}
+        public Ponto(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+}
---
+++ Trab(1)Lp2/ConsoleApplication1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double somaX = 0;
+            double somaY = 0;
+            int n = int.Parse(Console.ReadLine());
+            List<Ponto> pontos = new List<Ponto>();
+            
+            for( int i = 0; i < n; i++)
+            {
+                Ponto ponto = new Ponto();
+                pontos.Add(ponto);
+                pontos[i].X = double.Parse(Console.ReadLine());
+                pontos[i].Y = double.Parse(Console.ReadLine());
+                somaX += pontos[i].X;
+                somaY += pontos[i].Y;
+
+            }
+
+            Console.WriteLine("{0} e {1}", somaX / n, somaY / n);
+
+
+            
+
+
+            
+        }
+    }
+}
